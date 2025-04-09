a38763c4b5ccfd9107105b82856a9674853de147
DavidPach02 <davidjustin.pacheco@gmail.com>
2025-04-09 09:26:18 +0800

    Added autoscrolling text for scrolley contents
    - Removed wrapping of text in scrolley content
    - Added autoscrolling text for scrolley content every 3 secs

---
+++ assets/js/sidescrolltext.js
@@ -0,0 +1,44 @@
+const textContainers = document.getElementsByClassName('textscroller');
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function until(conditionFunction) {
+
+    const poll = resolve => {
+      if(conditionFunction()) resolve();
+      else setTimeout(_ => poll(resolve), 400);
+    }
+  
+    return new Promise(poll);
+}
+
+async function demo(el){
+    el.scrollLeft = 0;
+    await sleep(3000);
+
+    while(el.scrollLeft + el.offsetWidth <= el.scrollWidth - 1){
+        el.scrollTo(el.scrollLeft + 1, 0);
+        await sleep(50);
+    }
+
+    await until(_ => el.scrollLeft + el.offsetWidth >= el.scrollWidth - 1);
+    await sleep(3000);
+    demo(el);
+}
+
+Array.from(textContainers).forEach((el) => {
+    self.addEventListener('load', () => {
+        /*self.setTimeout(() => {
+            self.setInterval(() => {
+                const targetScrollWidth = el.scrollWidth;
+                if(el.scrollLeft + el.offsetWidth <= targetScrollWidth){
+                    el.scrollTo(el.scrollLeft + 1, 0);
+                }
+                //console.log(el.scrollLeft + el.offsetWidth);
+            }, 10);
+        }, 3000);*/
+        demo(el);
+    });
+});
