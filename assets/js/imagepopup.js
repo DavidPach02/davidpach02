d2b489d3cba16ad2a3790fa785829053b84e8a0c
DavidPach02 <davidjustin.pacheco@gmail.com>
2025-04-18 23:11:09 +0800

    Overhauled Extras Page
    - Rearranged Extras Page
    - Added main bar for Art and Video Editing with Photo Grid
    - Added side bar for Blog and Randomized Game Projects Section
    - Changed index-template.html to temp-index.html
    - Cleaned up imagepopup.js

--- assets/js/imagepopup.js
+++ assets/js/imagepopup.js
@@ -2,7 +2,6 @@
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("imagepopup");
 const imgs = document.getElementsByClassName('imagepopup');
 
 var modalImg = document.getElementById("img01");
