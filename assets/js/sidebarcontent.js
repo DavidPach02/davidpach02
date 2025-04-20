0455a2fcfef9aba83ad5ff7351ff5dcce9050779
DavidPach02 <davidjustin.pacheco@gmail.com>
2025-04-20 10:34:37 +0800

    Changed Sidebar Content population algorithm
    - Changed Sidebar content population to be populated on page load
    - Changed sidebar content population to accommodate page category population and page category exclusions
    - Updated relevant updated pages with the sidebar content
    - Changed css media rule from 1680px to 1920px

--- assets/js/sidebarcontent.js
+++ assets/js/sidebarcontent.js
@@ -1,24 +1,8 @@
-// Get page title and page category
-var pageTitle = document.title.split("-")
-var pageCategory = pageTitle[0].slice(0, -1)
-
-// Initialize all categories
-const categories = ["Video Games", "Game Jams", "Tabletop Games", "Art", "Video Editing"]
-
-// Remove current category
-let indexToRemove = categories.indexOf(pageCategory)
-categories.splice(indexToRemove, 1)
-
 // Initialize all lists -- Format [Name]|[Link]|[Image]
 // Video Games
-const videoGameCategory = [
+const professionalProjectsCategory = [
     "Assassin's Creed Mirage|vg-acmirage.html|images/scrolley/vg/scrolley-vg-acmirage.jpg",
-    "The Yellow Boat Reimagined|vg-tybr.html|images/scrolley/vg/scrolley-vg-theyellowboat.jpg",
-    "Nutrimania|vg-nutrimania.html|images/scrolley/vg/scrolley-vg-nutrimania.jpg",
     "Matt vs Aliens|vg-mattvsaliens.html|images/scrolley/vg/scrolley-vg-mattvsaliens.jpg",
-    "Rebelyon|vg-rebelyon.html|images/scrolley/vg/scrolley-vg-rebelyon.jpg",
-    "Nostalgia|vg-nostalgia.html|images/scrolley/vg/scrolley-vg-nostalgia.jpg",
-    "Connected: Summmer of '09|vg-cs09.html|images/scrolley/vg/scrolley-vg-cs09.jpg",
     "Skull and Bones|vg-snb.html|images/scrolley/vg/scrolley-vg-skullandbones.jpg"
 ]
 
@@ -33,14 +17,19 @@ const gameJamCategory = [
 ]
 
 // Tabletop Games
-const tabletopGameCategory = [
+const schoolProjectsCategory = [
+    "The Yellow Boat Reimagined|vg-tybr.html|images/scrolley/vg/scrolley-vg-theyellowboat.jpg",
+    "Nutrimania|vg-nutrimania.html|images/scrolley/vg/scrolley-vg-nutrimania.jpg",
+    "Rebelyon|vg-rebelyon.html|images/scrolley/vg/scrolley-vg-rebelyon.jpg",
+    "Nostalgia|vg-nostalgia.html|images/scrolley/vg/scrolley-vg-nostalgia.jpg",
+    "Connected: Summmer of '09|vg-cs09.html|images/scrolley/vg/scrolley-vg-cs09.jpg",
     "Ludens Dei|tg-ludensdei.html|images/scrolley/tg/scrolley-tg-ludensdei.jpg"
 ]
 
 // Art
 const artCategory = [
     "90's Japanese Setting|art-90sjapan.html|images/scrolley/art/scrolley-art-90sJapan.jpg",
-    "Uhk Aran|art-uhkanar.html|images/scrolley/art/scrolley-art-uhkanar.jpg",
+    "Uhk' Anar|art-uhkanar.html|images/scrolley/art/scrolley-art-uhkanar.jpg",
     "Nutrimania Art Compilation|art-nutriui.html|images/scrolley/art/scrolley-art-nutrimaniaui.jpg"
 ]
 
@@ -49,42 +38,110 @@ const videoEditingCategory = [
     "Team Comp or Meme Comp|vid-tcomc.html|images/scrolley/vid/scrolley-vid-tcomc.jpg"
 ]
 
-var categoriesPicked = "";
-for(let i = 0; i < 5; i++){
-    var randCategoryIndex = Math.floor(Math.random() * categories.length);
+// Initialize all categories
+const categories = ["Professional Projects", "Game Jams", "School Projects", "Art", "Video Editing"]
 
-    var targetArray = null;
-    if(categories[randCategoryIndex] == "Video Games"){
-        targetArray = videoGameCategory;
-    } 
-    else if (categories[randCategoryIndex] == "Game Jams"){
-        targetArray = gameJamCategory;
-    }
-    else if (categories[randCategoryIndex] == "Tabletop Games"){
-        targetArray = tabletopGameCategory;
-    }
-    else if (categories[randCategoryIndex] == "Art"){
-        targetArray = artCategory;
-    }
-    else if (categories[randCategoryIndex] == "Video Editing"){
-        targetArray = videoEditingCategory;
+
+function loadRandomGames(fillerContentCount, sameContentCount, exclusions){
+    // Get page title and page category
+    var pageTitle = document.title.split("-")
+    var pageCategory = pageTitle[0].slice(0, -1)
+
+    // Initialize final value of inner html
+    var categoriesPicked = "";
+
+    // Check exclusions
+    if (typeof exclusions !== "undefined"){
+        exclusions.forEach(element => {
+            for(let i = 0; i < categories.length; i++){
+                if (element === categories[i]){
+                    categories.splice(i, 1);
+                }
+            }
+        });
     }
 
-    var targetItem = GetRandomItemFromArray(targetArray)
-    var splittedInfo = targetItem.split("|")
+    // Get current category (-1 if no category found)
+    let indexToRemove = categories.indexOf(pageCategory)
+    // If same content count is defined then populate with same content based on number provided
+    if(typeof sameContentCount !== "undefined" && sameContentCount > 0){
+        var mainArray = GetArrayFromName(pageCategory);
+        var fileName = location.href.split("/").slice(-1);         
+        // Remove instance of page displayed
+        RemoveInstance(mainArray, fileName)
+
+        for(let i = 0; i < sameContentCount; i++){
+            var mainItem = GetRandomItemFromArray(mainArray);
+            var mainInfo = mainItem.split("|")
+
+            categoriesPicked += "<div class=\"col-4\"> <a href=\"" + mainInfo[1] + "\" class=\"image fit\"><img src=\"" + mainInfo[2] +"\" alt=\"\" /></a></div>"
+            categoriesPicked += "<div class=\"col-8\"><h4>" + pageCategory + "</h4><p><a href=\"" + mainInfo[1] + "\">" + mainInfo[0] +"</a></p></div>"
+
+            if (mainArray.length <= 0){
+                break
+            }
+        }
+    }
+
+    if (indexToRemove >= 0){
+        // Remove current category
+        categories.splice(indexToRemove, 1)
+    }
     
-    categoriesPicked += "<div class=\"col-4\"> <a href=\"" + splittedInfo[1] + "\" class=\"image fit\"><img src=\"" + splittedInfo[2] +"\" alt=\"\" /></a></div>"
-    categoriesPicked += "<div class=\"col-8\"><h4>" + categories[randCategoryIndex] + "</h4><p><a href=\"" + splittedInfo[1] + "\">" + splittedInfo[0] +"</a></p></div>"
 
-    if (targetArray.length <= 0){
-        categories.splice(randCategoryIndex, 1)
+    // Add filler content
+    for(let i = 0; i < fillerContentCount; i++){
+        var randCategoryIndex = Math.floor(Math.random() * categories.length);
+        console.log(randCategoryIndex + " " + categories.length);
+        var targetArray = GetArrayFromName(categories[randCategoryIndex])
+        var targetItem = GetRandomItemFromArray(targetArray)
+        var splittedInfo = targetItem.split("|")
+        
+        categoriesPicked += "<div class=\"col-4\"> <a href=\"" + splittedInfo[1] + "\" class=\"image fit\"><img src=\"" + splittedInfo[2] +"\" alt=\"\" /></a></div>"
+        categoriesPicked += "<div class=\"col-8\"><h4>" + categories[randCategoryIndex] + "</h4><p><a href=\"" + splittedInfo[1] + "\">" + splittedInfo[0] +"</a></p></div>"
+
+        if (targetArray.length <= 0){
+            categories.splice(randCategoryIndex, 1)
+        }
+    }
+
+    //alert(categoriesPicked);
+    //var targetHTML = "<div class=\"col-4\"> <a href=\"#\" class=\"image fit\"><img src=\"images/pic10.jpg\" alt=\"\" /></a></div>
+    //<div class=\"col-8\"><h4>Art</h4><p>Amet nullam fringilla nibh nulla convallis tique ante proin.</p></div>"
+    document.querySelector('#sample').innerHTML += categoriesPicked;
+}
+
+function RemoveInstance(targetArray, pageName){
+    for(let i = 0; i < targetArray.length; i++){
+        var info = targetArray[i].split("|");
+        console.log(pageName + " " + info[1]);
+
+        if (pageName == info[1]){
+            targetArray.splice(i, 1);
+        }
     }
 }
 
-//alert(categoriesPicked);
-//var targetHTML = "<div class=\"col-4\"> <a href=\"#\" class=\"image fit\"><img src=\"images/pic10.jpg\" alt=\"\" /></a></div>
-//<div class=\"col-8\"><h4>Art</h4><p>Amet nullam fringilla nibh nulla convallis tique ante proin.</p></div>"
-document.querySelector('#sample').innerHTML = categoriesPicked;
+function GetArrayFromName(name){
+    var targetArray = null;
+    if(name == "Professional Projects"){
+        targetArray = professionalProjectsCategory;
+    } 
+    else if (name == "School Projects"){
+        targetArray = schoolProjectsCategory;
+    }
+    else if (name == "Game Jams"){
+        targetArray = gameJamCategory;
+    }
+    else if (name == "Art"){
+        targetArray = artCategory;
+    }
+    else if (name == "Video Editing"){
+        targetArray = videoEditingCategory;
+    }
+
+    return targetArray;
+}
 
 function GetRandomItemFromArray(targetArray){
     var randIndex = Math.floor(Math.random() * targetArray.length);
